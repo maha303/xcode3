0d6e2021648cd0624bf67d1742295edd06c8dc27
maha303 <86849690+maha303@users.noreply.github.com>
2021-11-24 15:43:16 +0300

    Update Contents.swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,27 +4,33 @@ var arr = [Int]()
 for i in 1...255{
     arr.append(i)
 }
+func a(){
+    
 let index1=Int(arc4random_uniform(UInt32(arr.count)))
 let index2=Int(arc4random_uniform(UInt32(arr.count)))
 
-let temp = arr[index1]
-arr[index1]=arr[index2]
-arr[index2]=temp
-
-for _ in 0..<100{
+//let temp = arr[index1]
+//arr[index1]=arr[index2]
+//arr[index2]=temp
     
-    let index1=Int(arc4random_uniform(UInt32(arr.count)))
-    let index2=Int(arc4random_uniform(UInt32(arr.count)))
+    arr.swapAt(index1 , index2)
+}
 
-    let temp = arr[index1]
-    arr[index1]=arr[index2]
-    arr[index2]=temp
+for _ in 0..<100 {
+    
+    a()
+   // let index1=Int(arc4random_uniform(UInt32(arr.count)))
+   // let index2=Int(arc4random_uniform(UInt32(arr.count)))
+
+   // let temp = arr[index1]
+  //  arr[index1]=arr[index2]
+ //   arr[index2]=temp
 
 }
 print(arr)
 
 for i in  0..<arr.count{
-    if arr[i] == 24 {
+    if arr[i] == 42 {
         print("We found the answer to the Ultimate Question of Life, the Universe, and Everything at index  : \(i)")
         break;
     }
